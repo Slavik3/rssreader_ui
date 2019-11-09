fa24fbd87523e8f709d06f46baa50244a327d33c
Viacheslav Kozlov <slavikkozlov3@gmail.com>
2019-11-09 17:19:22 +0200

    put fields to class

---
+++ src/app/source.ts
@@ -0,0 +1,8 @@
+export class Source {
+  id: number;
+  sourceURL: string;
+  title: string;
+  description: string;
+  link: string;
+  pubDate: string;
+}
--- src/app/source/source.component.ts
+++ src/app/source/source.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { CustomFormsModule } from 'ng2-validation';
+import { Source} from '../source';
 
 @Component({
   selector: 'app-source',
@@ -8,54 +8,43 @@ import { CustomFormsModule } from 'ng2-validation';
   styleUrls: ['./source.component.scss']
 })
 export class SourceComponent implements OnInit {
+  newSource: Source = new Source();
+  editSource: Source = new Source();
   ids: number;
-  sourceURL: string;
-  title: string;
-  description: string;
-  link: string;
-  pubDate: string;
   sourceItems: any;
   editItem: any;
-
   newSourceURL: string;
-
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.editItem = '';
-
     this.httpClient.get('http://localhost:8080/sources/getAll').subscribe((data) => {
       this.sourceItems = data;
-      /*this.title = 'title';
-      this.description = 'description';
-      this.link = 'link';
-      this.pubDate = 'pubDate';*/
+      this.newSource.title = 'title';//default values
+      this.newSource.description = 'description';
+      this.newSource.link = 'link';
+      this.newSource.pubDate = 'pubDate';
     });
   }
 
-
-
   addSrc() {
     console.log('test');
-
     console.log(this.newSourceURL);
     const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/y;
-    const match = regex.exec(this.newSourceURL);
+    const match = regex.exec(this.newSource.sourceURL);
     if (match == null) {
-      console.log('ttt');
       (document.getElementById('src') as HTMLElement).style.borderBottom = '2px solid red';
     } else {
       const body = {
-      sourceURL: this.newSourceURL,
-      title: document.getElementById('newTitle').value,
-      description: document.getElementById('newDescription').value,
-      link: document.getElementById('newLink').value,
-      pubDate: document.getElementById('newPubDate').value,
+      sourceURL: this.newSource.sourceURL,
+      title: this.newSource.title,
+      description: this.newSource.description,
+      link: this.newSource.link,
+      pubDate: this.newSource.pubDate,
       };
       console.log(body);
       this.httpClient.post('http://localhost:8080/sources/add', body).subscribe();
-      this.newSourceURL = '';
       this.sourceItems.push(body);
+      this.newSource.sourceURL = '';
       // window.location.reload();
     }
 }
@@ -64,35 +53,47 @@ export class SourceComponent implements OnInit {
     console.log('delete');
     if (confirm('Are you sure you want to delete these source?')) {
       this.httpClient.delete(`http://localhost:8080/sources/${id1}`).subscribe();
-      window.location.reload();
+      //window.location.reload();
+      this.fetchData();
     }
+
   }
 
   open(editItem: any) {
     console.log('open');
-    console.log(editItem);
-    this.editItem = editItem;
+    this.editSource.sourceURL = editItem.sourceURL;
+    this.editSource.title = editItem.title;
+    this.editSource.description = editItem.description;
+    this.editSource.link = editItem.link;
+    this.editSource.pubDate = editItem.pubDate;
   }
 
   save(ob: any) {
     console.log('save');
-
-
     const body = {
       id: ob.id,
-      sourceURL: document.getElementById('modifiedSourceURL').value,/*this.sourceURL,*/
-      title: document.getElementById('title').value,/*this.title,*/
-      description: document.getElementById('description').value,/*this.description,*/
-      link: document.getElementById('link').value,/*this.link,*/
+      sourceURL: this.editSource.sourceURL,
+      title: this.editSource.title,
+      description: this.editSource.description,
+      link: this.editSource.link,
       hostname: ob.hostname,
-      pubDate: document.getElementById('pubDate').value,/*this.pubDate,*/
+      pubDate: this.editSource.pubDate,
     }
     console.log(body);
     this.httpClient.put(`http://localhost:8080/sources/${this.ids}`, body).subscribe();
-    
+    ob.sourceURL = this.editSource.sourceURL;
+    ob.title = this.editSource.title;
+    ob.description = this.editSource.description;
+    ob.link = this.editSource.link;
+    ob.pubDate = this.editSource.pubDate;
     //window.location.reload();
-}
 
+}
+  fetchData() {
+    this.httpClient.get('http://localhost:8080/sources/getAll').subscribe((data) => {
+      this.sourceItems = data;
+    });
+  }
 
 
 }
